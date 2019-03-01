12f525b680ae59ae2a0ecbf8c50d98859c26d433
Dariush Tasdighi <dariush.tasdighi@hotmail.com>
2019-03-01 15:20:23 +0330

    Add project files.

---
+++ Models/Documents/Readme.txt
@@ -0,0 +1,49 @@
+﻿قبل از اینکه اولین نسخه را به مشتری ارایه دهیم
+
+تست می‌کنیم که
+ConnectionString
+هم در پروژه اصلی و هم در پروژه
+Models
+نوشته شده و یکسان باشد
+
+ابتدا یک بار پروژه را اجرا کرده تا بانک اطلاعاتی ایجاد شود
+
+از طریق
+Tools --> Nuget Package Manager --> Package Manager Console
+شده Nuget وارد محیط
+
+از قسمت
+Default Project
+را انتخاب می‌کنیم Models پروژه
+
+(1)
+
+PM> Enable-Migrations
+
+(2)
+
+Edit Migrations\Configuration.cs File.
+
+(3)
+
+Note: The below codes should be in constructor of Migrations\Configuration.cs File
+
+AutomaticMigrationsEnabled = false;
+AutomaticMigrationDataLossAllowed = false;
+
+(4)
+
+In DatabaseContext.cs File
+
+Comment the below code:
+
+//System.Data.Entity.Database.SetInitializer
+//	(new DatabaseContextInitializerBeforeTheFirstRelease());
+
+(5)
+
+Then write the below code:
+
+System.Data.Entity.Database.SetInitializer
+	(new System.Data.Entity.MigrateDatabaseToLatestVersion
+		<DatabaseContext, Migrations.Configuration>());
---
+++ Models/Migrations/Configuration.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq;
+using System.Data.Entity;
+
+namespace Models.Migrations
+{
+	internal sealed class Configuration :
+		System.Data.Entity.Migrations.DbMigrationsConfiguration<DatabaseContext>
+	{
+		public Configuration() : base()
+		{
+			ContextKey = "Models.DatabaseContext";
+
+			AutomaticMigrationsEnabled = false;
+			AutomaticMigrationDataLossAllowed = false;
+		}
+
+		/// <summary>
+		/// دقت داشته باشید که این تابع همیشه اجرا می شود
+		/// حتی در صورتی که بانک اطلاعاتی وجود داشته باشد
+		/// </summary>
+		protected override void Seed(DatabaseContext databaseContext)
+		{
+			//base.Seed(databaseContext);
+
+			//if (databaseContext.People.Count() != 0)
+			//{
+			//	return;
+			//}
+
+			// با تشکر از آقای فرشاد ربیعی
+			if (databaseContext.People.Any())
+			{
+				return;
+			}
+
+			try
+			{
+				DatabaseContextInitializer.Seed(databaseContext);
+			}
+			catch
+			{
+			}
+			//catch (System.Exception ex)
+			//{
+			//	Dtx.LogHandler.Report(GetType(), null, ex);
+			//}
+		}
+	}
+}
